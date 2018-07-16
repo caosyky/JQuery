087bcf3b9f48a4befb55ca7161b2e938e5aa9425
Sy Ky <caosyky.1994@gmail.com>
2018-07-17 00:01:14 +0700

    fix responsive

--- navnar whatsapp/1.js
+++ navnar whatsapp/1.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
 	$(".onclick1").click(function(event) {
-		$(".onclick1").hide(100);
-		$(".onclick2").show(500);
 		$('.themenu-mobi').css('transform', 'translate(0, 0)');
 		$('.themenu-mobi').css('-webkit-transform', 'translate(0, 0)');
 	});
 	$(".onclick2").click(function(event) {
-		$(".onclick2").hide(100);
-		$(".onclick1").show(500);
 		$('.themenu-mobi').css('transform', 'translate(100%, 0)');
 		$('.themenu-mobi').css('-webkit-transform', 'translate(100%, 0)');
 	});
